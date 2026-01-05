d3c75fa82e110e1b35786ac9621c97dfeb185e51
Kim Valkama <hello@valkama.kim>
2026-01-05 23:06:15 +0200

    Network in done

--- network_in_out.c
+++ network_in_out.c
@@ -8,27 +8,24 @@ int main() {
 /* Change enp5s0 to what your network interface is called */
   FILE *in_current = fopen("/sys/class/net/enp5s0/statistics/rx_bytes", "r");
 
-  /* current = total bytes in since boot, diff = difference in total since boot and last time saved to tmp, last = bytes in tmp before overwriting */
+  /* current = total bytes in since boot, last = bytes in tmp before overwriting */
   /* i is for checking if the if statements have triggered */
   char bytes_in_current[STRSIZE];
-  char bytes_in_diff[STRSIZE];
   char bytes_in_last[STRSIZE];
   int i = 0;
 
-  /* Get total bytes since boot, return NA if file wont open */
+  /* Get total bytes since boot, increment i if file not available */
   if(in_current == NULL) {
-    strncpy(bytes_in_diff, "NA\n", 3);
     i++;
   }
   else {
     fgets(bytes_in_current, STRSIZE, in_current);
     fclose(in_current);
 
-    /* Get last checked bytes from tmp file, return NA and create file if it does not exist */
+    /* Get last checked bytes from tmp file, increment i and create file, if file not available */
     FILE *in_last_r = fopen("./tmp/net_in_tmp", "r");
 
     if(in_last_r == NULL) {
-      strncpy(bytes_in_diff, "NA\n", 3);
       FILE *in_last_w = fopen("./tmp/net_in_tmp", "w");
       fclose(in_last_w);
       i++;
@@ -48,10 +45,14 @@ int main() {
   /* Check if bytes_in_diff is still empty, and calculate diff */
 
   /*TODO! Check if value of i is 0, and if so convert last and current to long long, calculate diff and convert to K, otherwise print diff as is */
- 
-  /*DEBUG: */
-//  printf("%d\n", i);
-//  printf("%s/%s\n", bytes_in_current, bytes_in_last);
+  if(i < 1) {
+    unsigned long long converted_bytes_in_current = strtoul(bytes_in_current, NULL, 10);
+    unsigned long long converted_bytes_in_last = strtoul(bytes_in_last, NULL, 10);
+    int converted_bytes_diff = (converted_bytes_in_current - converted_bytes_in_last) / 1024;
+    printf("%d", converted_bytes_diff);
+  } else {
+    printf("IN NA\n");
+  }
 
   return 0;
 }
