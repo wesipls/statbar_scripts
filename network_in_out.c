1adfa7c148d78fc0ed810da1921cfee18759781e
Kim Valkama <hello@valkama.kim>
2026-01-06 22:52:29 +0200

    Cleaned up network_in_out.c

--- network_in_out.c
+++ network_in_out.c
@@ -4,55 +4,56 @@
 
 #define STRSIZE 64
 
+void read_bytes(int *counter, char *bytes, char *read_file); /* Read files from rx_bytes and define to variable */
+void write_bytes(int *counter, char *bytes, char *read_file); /* Write rx_bytes from variable to tmp file for last received bytes tracking */
+int bytes_diff(int counter, char *bytes_current, char *bytes_last); /* Calculate last received bytes and current received bytes for difference in recived bytes for last x seconds */
+
 int main() {
-/* Change enp5s0 to what your network interface is called */
-  FILE *in_current = fopen("/sys/class/net/enp5s0/statistics/rx_bytes", "r");
+  
+  char rx_bytes_current[] = "/sys/class/net/enp5s0/statistics/rx_bytes"; /* File to fetch rx_bytes from */
+  char rx_bytes_last[] = "./tmp/net_in_tmp"; /* tmp file to store rx_bytes in */
+  char bytes_in_current[STRSIZE]; /* String of bytes form rx_bytes */
+  char bytes_in_last[STRSIZE]; /* Last copied value to tmp file from rx_bytes */
+  int i = 0; /* int for counting failed file functions */
 
-  /* current = total bytes in since boot, last = bytes in tmp before overwriting */
-  /* i is for checking if the if statements have triggered */
-  char bytes_in_current[STRSIZE];
-  char bytes_in_last[STRSIZE];
-  int i = 0;
+  read_bytes(&i, bytes_in_current, rx_bytes_current);
+  read_bytes(&i, bytes_in_last, rx_bytes_last); 
+  write_bytes(&i, bytes_in_current, rx_bytes_last);
+ 
+  int converted_bytes_diff = bytes_diff(i, bytes_in_current, bytes_in_last);  /* Difference in bytes_in_current and bytes_in_last */
 
-  /* Get total bytes since boot, increment i if file not available */
-  if(in_current == NULL) {
-    i++;
-  }
-  else {
-    fgets(bytes_in_current, STRSIZE, in_current);
-    fclose(in_current);
 
-    /* Get last checked bytes from tmp file, increment i and create file, if file not available */
-    FILE *in_last_r = fopen("./tmp/net_in_tmp", "r");
-
-    if(in_last_r == NULL) {
-      FILE *in_last_w = fopen("./tmp/net_in_tmp", "w");
-      fclose(in_last_w);
-      i++;
-    }
-    else {
-    fgets(bytes_in_last, STRSIZE, in_last_r);
-    fclose(in_last_r);
-
-    /* Write new last value to tmp file */
-    FILE *in_last_w = fopen("./tmp/net_in_tmp", "w");
-    fputs(bytes_in_current, in_last_w);
-    fclose(in_last_w);
-
-    }
-  }
-
-  /* Check if bytes_in_diff is still empty, and calculate diff */
-
-  /*TODO! Check if value of i is 0, and if so convert last and current to long long, calculate diff and convert to K, otherwise print diff as is */
-  if(i < 1) {
-    unsigned long long converted_bytes_in_current = strtoul(bytes_in_current, NULL, 10);
-    unsigned long long converted_bytes_in_last = strtoul(bytes_in_last, NULL, 10);
-    int converted_bytes_diff = (converted_bytes_in_current - converted_bytes_in_last) / 1024;
-    printf("%d", converted_bytes_diff);
-  } else {
-    printf("IN NA\n");
-  }
-
+//DEBUG
+//  printf("%d\n", i); 
+//  printf("%s / %s\n", bytes_in_current, bytes_in_last);
+  printf("%d\n", converted_bytes_diff);
   return 0;
+};
+
+
+void read_bytes(int *counter, char *bytes, char *read_file) {
+  FILE *file = fopen(read_file, "r");
+  if(file == NULL) {
+    (*counter)++;
+  } else {
+    fgets(bytes, STRSIZE, file);
+    fclose(file);
+  }
+}
+
+void write_bytes(int *counter, char *bytes, char *read_file) {
+  FILE *file = fopen(read_file, "w");
+  fputs(bytes, file);
+  fclose(file);
+}
+
+int bytes_diff(int counter, char *bytes_current, char *bytes_last) {
+  if(counter < 1) {
+    unsigned long long converted_bytes_current = strtoul(bytes_current, NULL, 10);
+    unsigned long long converted_bytes_last = strtoul(bytes_last, NULL, 10);
+    int converted_bytes_diff = (converted_bytes_current - converted_bytes_last) / 1024;
+    return converted_bytes_diff;
+  } else {
+    return 0;
+  }
 }
