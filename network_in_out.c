bff0da32dffefc114a81c2e15fe1b2ee0190fc0c
Kim Valkama <hello@valkama.kim>
2026-01-07 23:06:18 +0200

    Added formatting for output, needs UTF-8 compatibility

--- network_in_out.c
+++ network_in_out.c
@@ -12,22 +12,33 @@ int main() {
   
   char rx_bytes_current[] = "/sys/class/net/enp5s0/statistics/rx_bytes"; /* File to fetch rx_bytes from */
   char rx_bytes_last[] = "./tmp/net_in_tmp"; /* Tmp file to store rx_bytes in */
+  char tx_bytes_current[] = "/sys/class/net/enp5s0/statistics/tx_bytes"; /* File to fetch tx_bytes from */
+  char tx_bytes_last[] = "./tmp/net_out_tmp"; /* Tmp file to store tx_bytes in */
+  
   char bytes_in_current[STRSIZE]; /* String of bytes form rx_bytes */
   char bytes_in_last[STRSIZE]; /* Last copied value to tmp file from rx_bytes */
+  char bytes_out_current[STRSIZE]; /* String of bytes form tx_bytes */
+  char bytes_out_last[STRSIZE]; /* Last copied value to tmp file from tx_bytes */
+
   int i = 0; /* Int for counting failed file functions */
   int time_diff = 5; /* Time since last diff check, just set it to the interval the script will run on, or use the time header to check tmp file filestamp */
 
   read_bytes(&i, bytes_in_current, rx_bytes_current);
   read_bytes(&i, bytes_in_last, rx_bytes_last); 
   write_bytes(&i, bytes_in_current, rx_bytes_last);
- 
-  int converted_bytes_diff = bytes_diff(i, time_diff, bytes_in_current, bytes_in_last);  /* Difference in bytes_in_current and bytes_in_last */
+  int converted_in_bytes_diff = bytes_diff(i, time_diff, bytes_in_current, bytes_in_last);  /* Difference in bytes_in_current and bytes_in_last */
 
+  i = 0; /* Resetting i for counting bytes out */
+
+  read_bytes(&i, bytes_out_current, tx_bytes_current);
+  read_bytes(&i, bytes_out_last, tx_bytes_last); 
+  write_bytes(&i, bytes_out_current, tx_bytes_last);
+  int converted_out_bytes_diff = bytes_diff(i, time_diff, bytes_out_current, bytes_out_last);  /* Difference in bytes_in_current and bytes_in_last */
 
 //DEBUG
 //  printf("%d\n", i); 
 //  printf("%s / %s\n", bytes_in_current, bytes_in_last);
-  printf("%d\n", converted_bytes_diff);
+  printf("\u21F5 %dK/%dK\n", converted_in_bytes_diff, converted_out_bytes_diff);
   return 0;
 };
 
