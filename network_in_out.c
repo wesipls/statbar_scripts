246abf1c7147f7150a486ab669e23c4a52a72a56
Kim Valkama <hello@valkama.kim>
2026-01-08 23:48:05 +0200

    added output as megabytes for 1024+ bytes output

--- network_in_out.c
+++ network_in_out.c
@@ -8,7 +8,7 @@
 
 void read_bytes(int *counter, char *bytes, char *read_file); /* Read files from rx_bytes and define to variable */
 void write_bytes(char *bytes, char *read_file); /* Write rx_bytes from variable to tmp file for last received bytes tracking */
-int bytes_diff(int counter, int time, char *bytes_current, char *bytes_last); /* Calculate last received bytes and current received bytes for difference in recived bytes for last x seconds */
+void bytes_diff(int counter, int time, char *bytes_current, char *bytes_last, char *converted_bytes); /* Calculate last received bytes and current received bytes for difference in recived bytes for last x seconds */
 
 int main() {
   
@@ -19,8 +19,10 @@ int main() {
   
   char bytes_in_current[STRSIZE]; /* String of bytes form rx_bytes */
   char bytes_in_last[STRSIZE]; /* Last copied value to tmp file from rx_bytes */
+  char converted_in_bytes_diff[STRSIZE]; /* String of converted rx difference in kilobytes or megabytes */
   char bytes_out_current[STRSIZE]; /* String of bytes form tx_bytes */
   char bytes_out_last[STRSIZE]; /* Last copied value to tmp file from tx_bytes */
+  char converted_out_bytes_diff[STRSIZE]; /* String of converted tx difference in kilobytes or megabytes */
 
   int i = 0; /* Int for counting failed file functions */
   int time_diff = 5; /* Time since last diff check, just set it to the interval the script will run on, or use the time header to check tmp file filestamp */
@@ -28,16 +30,17 @@ int main() {
   read_bytes(&i, bytes_in_current, rx_bytes_current);
   read_bytes(&i, bytes_in_last, rx_bytes_last); 
   write_bytes(bytes_in_current, rx_bytes_last);
-  int converted_in_bytes_diff = bytes_diff(i, time_diff, bytes_in_current, bytes_in_last);  /* Difference in bytes_in_current and bytes_in_last */
 
   i = 0; /* Resetting i for counting bytes out */
 
   read_bytes(&i, bytes_out_current, tx_bytes_current);
   read_bytes(&i, bytes_out_last, tx_bytes_last); 
   write_bytes(bytes_out_current, tx_bytes_last);
-  int converted_out_bytes_diff = bytes_diff(i, time_diff, bytes_out_current, bytes_out_last);  /* Difference in bytes_in_current and bytes_in_last */
 
-  printf("\u21F5 %dK/%dK\n", converted_in_bytes_diff, converted_out_bytes_diff);
+  bytes_diff(i, time_diff, bytes_in_current, bytes_in_last, converted_in_bytes_diff);
+  bytes_diff(i, time_diff, bytes_out_current, bytes_out_last, converted_out_bytes_diff);
+  printf("\u21F5 %s/%s", converted_in_bytes_diff, converted_out_bytes_diff);
+
   return 0;
 };
 
@@ -58,13 +61,16 @@ void write_bytes( char *bytes, char *read_file) {
   fclose(file);
 }
 
-int bytes_diff(int counter, int time, char *bytes_current, char *bytes_last) {
+void bytes_diff(int counter, int time, char *bytes_current, char *bytes_last, char *converted_bytes) {
   if(counter < 1) {
     unsigned long long converted_bytes_current = strtoul(bytes_current, NULL, 10);
     unsigned long long converted_bytes_last = strtoul(bytes_last, NULL, 10);
     int converted_bytes_diff = ((converted_bytes_current - converted_bytes_last) / 1024) / 5;
-    return converted_bytes_diff;
-  } else {
-    return 0;
+    if (converted_bytes_diff > 1024) {
+      float tmp_result = (float) converted_bytes_diff / (float) 1024;
+      sprintf(converted_bytes, "%.1fM", tmp_result);   
+    } else {
+      sprintf(converted_bytes, "%dK", converted_bytes_diff);
+    }
   }
 }
