28add6939ced2e744188fafd7f11e73e42b9a078
Kim Valkama <hello@valkama.kim>
2026-01-16 21:16:43 +0200

    Added some clarity to cpu.c, copilot commenting not very gucci

--- cpu.c
+++ cpu.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/* Retrieves CPU stats from /proc/stat */
 void read_proc_stat(unsigned long *prev_total, unsigned long *prev_idle,
                     unsigned long *current_total, unsigned long *current_idle) {
   FILE *file = fopen("/proc/stat", "r");
@@ -11,24 +12,19 @@ void read_proc_stat(unsigned long *prev_total, unsigned long *prev_idle,
   char line[256];
 
   if (fgets(line, sizeof(line), file)) {
-    /* Variables to hold CPU usage data */
     unsigned long user, nice, system, idle, iowait, irq, softirq, steal, guest,
         guest_nice;
-
-    /* Parse the first line of /proc/stat into variables */
     sscanf(line, "cpu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu", &user, &nice,
            &system, &idle, &iowait, &irq, &softirq, &steal, &guest,
            &guest_nice);
-
-    /* Save the values to a temporary file */
     *current_total = user + nice + system + irq + softirq + steal + guest +
                      guest_nice + idle + iowait;
     *current_idle = idle + iowait;
   }
-
   fclose(file);
 }
 
+/* Retrieves previous values from tmp file*/
 void read_prev_usage(unsigned long *prev_total, unsigned long *prev_idle) {
   FILE *tmp_file = fopen("/tmp/cpu_usage", "r");
   if (tmp_file) {
@@ -38,7 +34,7 @@ void read_prev_usage(unsigned long *prev_total, unsigned long *prev_idle) {
     fclose(tmp_file);
   }
 }
-
+/* Calculates and prints CPU usage */
 void calculate_cpu_usage(unsigned long prev_total, unsigned long prev_idle,
                          unsigned long current_total,
                          unsigned long current_idle) {
@@ -48,14 +44,13 @@ void calculate_cpu_usage(unsigned long prev_total, unsigned long prev_idle,
       ((double)(total_final - total_idle) * 100) / (double)total_final;
   printf("CPU: %.2f%%\n", usage);
 }
-
+/* Writes current values to tmp file */
 void write_tmp_file(unsigned long current_total, unsigned long current_idle) {
   FILE *file = fopen("/tmp/cpu_usage", "w");
   if (!file) {
     perror("Failed to open /tmp/cpu_usage");
     return;
   }
-
   fprintf(file, "%lu %lu\n", current_total, current_idle);
   fclose(file);
 }
