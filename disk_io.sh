3eb054e5d4097fe41d2d0f3524dfbfb99a37041f
Kim Valkama <hello@valkama.kim>
2026-01-22 15:44:41 +0200

    Fixed disk io to work with any disk as long as uuid is provided

--- disk_io.sh
+++ disk_io.sh
@@ -3,16 +3,18 @@
 # Usage: ./disk_io.sh
 # Example output: ↑ 1.2M / ↓ 3.4M
 #
-# Fetces disk io statistics from /sys/class/block/sdb/stat, saves old values in /tmp/disk_in and /tmp/disk_out.
-# Measures the sdb disk IO in kb/mb per second.
+# Fetches disk io statistics from /sys/class/block/$disk/stat, saves old values in /tmp/disk_in and /tmp/disk_out.
+# Measures the $disk disk IO in kb/mb per second.
 #
 # Assumes this script is executed every 5 seconds, if you plan on running it with another interval update interval variable.
-# Assumes sdb is the disk to monitor, change disk variable if needed.
+# Assumes $uuid is the uuid of your disk, wich is probably not the case, you can check your disk uuid from /dev/disk/by-uuid.
 #
-# Should work on any Linux as long as /sys/class/block/sdb/stat is available.
+# Should work on any Linux as long as /sys/class/block/$disk/stat is available.
 
 interval=5
-disk=sdb
+uuid="8b33bb8a-2621-4d06-9645-9809f09e660b"
+
+disk=$(readlink /dev/disk/by-uuid/$uuid | xargs basename)
 
 to_human_readable() {
   local bytes=$1
@@ -23,35 +25,27 @@ to_human_readable() {
   fi
 }
 
-# Get saved initial disk statistics
 if [[ -f /tmp/disk_in && -f /tmp/disk_out ]]; then
   start_read=$(cat /tmp/disk_in)
   start_write=$(cat /tmp/disk_out)
 else
-  # If files don't exist, initialize them
   start_read=$(cat /sys/class/block/$disk/stat | awk '{print $3}')
   start_write=$(cat /sys/class/block/$disk/stat | awk '{print $7}')
   echo $start_read >/tmp/disk_in
   echo $start_write >/tmp/disk_out
-  echo "No previous data available. Initialization complete."
   exit 0
 fi
 
-# Get current disk statistics
 end_read=$(cat /sys/class/block/$disk/stat | awk '{print $3}')
 end_write=$(cat /sys/class/block/$disk/stat | awk '{print $7}')
 
-# Calculate the difference in bytes per second
 read_diff=$(((end_read - start_read) * 512 / interval))
 write_diff=$(((end_write - start_write) * 512 / interval))
 
-# Save the current data for the next run
 echo $end_read >/tmp/disk_in
 echo $end_write >/tmp/disk_out
 
-# Convert the differences to human-readable format
 read_rate=$(to_human_readable $read_diff)
 write_rate=$(to_human_readable $write_diff)
 
-# Display the results
 echo "↑ $read_rate / ↓ $write_rate"
